f22eacfb2e2eda2cc424023592172068e20da568
sttrackstar <sonalt321@gmail.com>
2026-06-08 06:05:08 -0400

    Assignment completed

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,18 +1,37 @@
-import { useOutletContext } from "react-router-dom"
+import { useOutletContext, Link, useParams } from "react-router-dom";
 
-function MovieList() {
-  const [director] = useOutletContext()
+const MovieList = () => {
+    const outlet = useOutletContext();
+    const directors = outlet?.directors ?? [];
+    const { directorId } = useParams();
 
-  // Test suite expects this EXACT text
-  if (!director) return <h2>Director not found</h2>
+    const director = directors.find((d) => String(d.id) === String(directorId)) || null;
 
-  return (
-    <ul>
-      {director.movies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-    </ul>
-  )
+    if (!director) {
+        return <h2>Director not found.</h2>
+    }
+
+    if(!director.movies || director.movies.length === 0) {
+        return (
+            <div>
+                <p>No movies available found for {director.name}</p>
+                <Link to={`movies/new`}>Add the first movie</Link>
+            </div>
+        );
+    }
+        return (
+            <div>
+                <h3>{director.name}'s Movies:</h3>
+                <ul>
+                    {director.movies.map((movie) => (
+                    <li key={movie.id}>
+                        <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+                    </li>
+                    ))}
+                </ul>
+                <Link to={`movies/new`}>Add New Movie</Link>
+            </div>
+        );
 }
 
-export default MovieList
+export default MovieList;
